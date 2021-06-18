b48039c490c0b73ef9a07ea623480a0fc6724981
lalldredge7622 <lalldredge7622@uinta1.org>
2021-06-18 13:53:58 -0600

    Update oove.sh

--- scripts/oove.sh
+++ scripts/oove.sh
@@ -2,15 +2,41 @@
 #ensure output of system tools in en-us for parsing
 export LC_ALL=C
 #where the crap is kept
-script_url="
+script_url="https://raw.githubusercontent.com/loginagohacker/oove/main/scripts/"
 #set working dir
 if grep -q "Chrom" /etc/lsb-release ; then
 	# needed for OneOs v1+
-	mkdir -p /usr/local/bin
-	cd /usr/local/bin
+	mkdir -p /usr/local/oove
+	cd /usr/local/oove
 else
 	cd /tmp
 fi
 
 #get support scripts
-echo -e "\nDownloading support files..."
+echo -e "Downloading support files..."
+rm -rf install.sh >/dev/null 2>&1
+rm -rf core.sh >/dev/null 2>&1
+rm -rf i3-dep.sh >/dev/null 2>&1
+curl -sLO ${script_url}install.sh
+rc0=$?
+curl -sLO ${script_url}core.sh
+rc1=$?
+curl -sLO ${script_url}i3-dep.sh
+rc2=$?
+if [[ $rc0 -ne 0 || $rc1 -ne 0 || $rc2 -ne 0 ]]; then
+	echo -e "Error downloading one or more required files; cannot continue"
+	exit 1
+fi
+
+source ./install.sh
+source ./core.sh
+source ./i3-dep.sh
+
+#set working dir
+cd /tmp
+
+#do setup stuff
+prelim_setup || exit 1
+
+#show menu
+
